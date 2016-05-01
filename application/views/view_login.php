855d76dd252f861c86a87db7368a90df7d0804bd
Kristian Indra <kristian.indra.1412@gmail.com>
2016-05-01 09:06:08 +0700

    Bootstrap

---
+++ application/views/view_login.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Dokter Gigi</title>
+
+	<style type="text/css">
+
+	</style>
+</head>
+<body>
+
+<table>
+  <tr>
+    <td><input placeholder="Username" id="login_username" name="login_username" autocomplete="off" value=""></td>
+    <td></td>
+  <tr>
+    <td><input placeholder="Password" type="password" id="login_password" name="login_password"> </td>
+  </tr>
+</table>
+
+<div id="container">
+	<h1>Welcome to CodeIgniter!</h1>
+
+	<div id="body">
+		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+
+		<p>If you would like to edit this page you'll find it located at:</p>
+		<code>application/views/welcome_message.php</code>
+
+		<p>The corresponding controller for this page is found at:</p>
+		<code>application/controllers/Welcome.php</code>
+
+		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+	</div>
+
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+</div>
+
+</body>
+</html>
