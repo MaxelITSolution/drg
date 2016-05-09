f2525091c34e4d73d55edf4c571a7ac569c5cb07
Kristian Indra <kristian.indra.1412@gmail.com>
2016-05-09 23:31:25 +0700

    Perubahan: Database, replace dengan yang baru di folder database pasien_id -> pasien_kode
    tak tambahi route, jadi klo akses bisa dengan
    drg/index.php/resepsionis

--- application/controllers/controller_index.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Controller_index extends CI_Controller {
-    public function index()
-    {
-        $this->load->view('view_login');
-    }
-}
